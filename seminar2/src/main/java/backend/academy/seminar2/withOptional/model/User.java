2a4c462ba2469b5c66674b40b881ffdaa8fe30ae
Ivan Korelin <i.korelin@tinkoff.ru>
2025-09-12 11:03:44 +0500

    seminar 2

---
+++ seminar2/src/main/java/backend/academy/seminar2/withOptional/model/User.java
@@ -0,0 +1,45 @@
+package backend.academy.seminar2.withOptional.model;
+
+public class User {
+    private final int id;
+    private final String name;
+    private final String email;
+
+    public User(int id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+    /*
+    Метод для создания нового экземпляра класса
+    вместо изменения существующего
+    */
+    public User withName(String name) {
+        return new User(this.id, name, this.email);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+            "id=" + id +
+            ", name='" + name + '\'' +
+            ", email='" + email + '\'' +
+            '}';
+    }
+}
+
+
+
+
