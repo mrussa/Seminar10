10a8a8932016b7c9acfce4877daa166e4f591c59
Mystic Alien <mysticalien@MacBook-Pro-Mystic.local>
2025-11-28 05:29:59 +0300

    добавила модификаторы доступа в PlayerState

--- seminar10/src/main/java/backend/academy/seminar10/refactor_tasks/game/finish/PlayerState.java
+++ seminar10/src/main/java/backend/academy/seminar10/refactor_tasks/game/finish/PlayerState.java
@@ -1,14 +1,36 @@
 package backend.academy.seminar10.refactor_tasks.game;
 
 public class PlayerState {
-    final String name;
-    int purse;
-    int place;
-    State state = State.FREE;
+    private final String name;
+    private int purse;
+    private int place;
+    private State state = State.FREE;
 
-    PlayerState(String name) {
+    public PlayerState(String name) {
         this.name = name;
     }
 
-    enum State {PENALTY, FREE}
+    public String getName() {
+        return name;
+    }
+    public int getPurse() {
+        return purse;
+    }
+    public void setPurse(int purse) {
+        this.purse = purse;
+    }
+    public int getPlace() {
+        return place;
+    }
+    public void setPlace(int place) {
+        this.place = place;
+    }
+    public State getState() {
+        return state;
+    }
+    public void setState(State state) {
+        this.state = state;
+    }
+
+    public enum State {PENALTY, FREE}
 }
